d361a485e6b8624164cd57533fd89f6e646d3e3a
Andersonisraellt <202509076091@alunos.estacio.br>
2025-09-16 00:45:27 +0000

    Inclusão dos parâmetros da carta 01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,13 @@
 int main(){
   printf("Jogo super trunfo \n");
   // Carta numero 01
-  char Estado =  
-  char Código da carta [20] =  
-  char Nome da cidade [20] = 
-  int População = 
-  float Área em km² =
-  float PIB = 
-  int Número de pontos turísticos = 
+ char Estado = 'Rio Grande do Sul'; 
+ char Código da carta[20] = "A01" 
+ char Nome da cidade[20] = "Esteio";
+ int População = 76137;
+ float Área em km² =26.676;
+ float PIB = 3.700000000;
+ int Número de pontos turísticos = 7;
   
   // Carta numero 02
   char Estado =
