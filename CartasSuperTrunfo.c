192f9d26b7fae3dd6217e72c0a7f15ca14c84e25
Andersonisraellt <202509076091@alunos.estacio.br>
2025-09-16 01:35:04 +0000

    Recriando novamente para correção de erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
-
-int main(){
-  
-  printf("Jogo super trunfo \n");
-  
+int main() {
+  printf("Super trunfo \n");
   // Carta numero 01
- char Estado = 'Rio Grande do Sul'; 
- char Código da carta[20] = "A01" 
- char Nome da cidade[20] = "Esteio";
- int População = 76.137;
- float Área em km² =26.676;
- float PIB = 3.700000000;
- int Número de pontos turísticos = 07;
   
-  // Carta numero 02
-  char Estado = 'Paraná'; 
- char Código da carta[20] = "A02" 
- char Nome da cidade[20] = "Maringá";
- int População = 429.660;
- float Área em km² = 487.012;
- float PIB = 22.660000000;
- int Número de pontos turísticos = 06;
-  
+  char  
+  char  
+  char 
+  int 
+  float 
+  float 
+  int 
 
-  return 0;
+
 }
