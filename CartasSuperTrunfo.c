02cbdf050f20b0a7d9e94dbc8235acfc4c0b2a6a
Andersonisraellt <202509076091@alunos.estacio.br>
2025-09-16 00:43:56 +0000

    Criando as cartas 01/02

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main() {
-printf("Jogo super trunfo \n");
+int main(){
+  printf("Jogo super trunfo \n");
+  // Carta numero 01
+  char Estado =  
+  char Código da carta [20] =  
+  char Nome da cidade [20] = 
+  int População = 
+  float Área em km² =
+  float PIB = 
+  int Número de pontos turísticos = 
+  
+  // Carta numero 02
+  char Estado =
+  char Código da carta [20] = 
+  char Nome da cidade [20] =
+  int População =
+  float Área em km² =
+  float PIB =
+  int Número de pontos turísticos =
 
   return 0;
 }
