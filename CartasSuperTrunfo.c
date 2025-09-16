099b6c19ef67896be80367f2c2a3ba25c8b1b431
Andersonisraellt <202509076091@alunos.estacio.br>
2025-09-16 23:07:56 +0000

    Definicão de arrays e entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 
-int  main() {
-  //Variáveis 
-    char estado[30];
-    char cidade[30];
-    char codigo[10];
-    int populacao;
-    float area;
-    float pib;
-    int numerodepontosturisticos;
-  
-    int quantidade = 2;
-    for (int i = 0; i < quantidade; i++) {
+int main(){
+  //Definição de arrays para inclusão das cartas
+
+  char estado[20];
+  char cidade[20];
+  char codigo[20];
+  int população;
+  float area;
+  float pib;
+  int numerodepontosturisticos;
+
+  int quantidade =3;
+  for (int i = 0; i <quantidade; i++) {
 
     // Entrada de dados
-    printf("Digite o estado: \n");
-    scanf("%29s", estado);
-    printf("Digite a cidade: \n");
-    scanf("%29s", cidade);
-    printf("Digite o código: \n");
-    scanf("%9s", codigo);
-    printf("Digite a população: \n");
-    scanf("%d", &populacao);
-    printf("Digite a área: \n");
-    scanf("%f", &area);
-    printf("Digite o PIB: \n");
-    scanf("%f", &pib);
-    printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &numerodepontosturisticos);
-    
-    // Impressão
-    
-    printf("DADOS DA CARTA\n");
-    printf("O estado é: %s\n", estado);
-    printf("A cidade é: %s\n", cidade);
-    printf("O código é: %s\n", codigo);
-    printf("A população é: %d\n", populacao);
-    printf("A área é: %.2f\n", area);
-    printf("O pib é: %.2f\n", pib);
-    printf("O número de pontos turísticos é: %d\n", numerodepontosturisticos);
-    printf("-----------------------------\n");
-    }
-    return 0;
+  printf("Digite o estado: \n");
+  scanf("%29s", estado);
+
+  printf("Digite a cidade: \n");
+  scanf("%29s", cidade);
+
+  printf("Digite o código: \n");
+  scanf("%9s", codigo);
+
+  printf("Digite a população: \n");
+  scanf("%d", &populacao);
+
+  pritf("Digite a área: \n");
+  scanf("%f, &area");
+
+  printf("Digite o PIB: \n");
+  scanf("%f", &pib);
+
+  printf("Digite o número de pontos tirísticos: \n");
+  scanf("%d", &numerodepontosturisticos);
+
+
+   
+  }
 }
+  
+    
+
+   
