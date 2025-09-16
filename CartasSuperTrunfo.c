d16496ce212a4b216399eafb99de5a78236de6d3
Andersonisraellt <202509076091@alunos.estacio.br>
2025-09-16 02:18:57 +0000

    Corrigindo cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,15 @@
 int main() {
   printf("Super trunfo \n");
 
-  
- 
+  // criação das cartas
+
+//Carta numero 01
+char Estado;  
+char Codigodacarta[20];
+char Nomedacidade[20];
+int Populacao; 
+float Areaemkm²; 
+float PIB; 
+int numerodepontosturistico;
 
 }
